bf6bd00422d926a63733e8b05a2e888d13f056cf
Chandan Laxminarayan Hegde <chandanhegden@gmail.com>
2023-03-03 07:35:25 +0530

    update

--- valid-subsequence.cpp
+++ valid-subsequence.cpp
@@ -1,28 +1,20 @@
-#include <iostream>
+include <iostream>
 #include <bits/stdc++.h>
 
 using namespace std;
 
 bool isValidSubsequence(vector<int> array, vector<int> sequence) {
-  // Write your code here.
-
-  if (array.size() < sequence.size()){
+ 
+  if (array.size() < sequence.size())
     return false;
-  }
-
-  vector<int> :: iterator it1 = array.begin();
-  vector<int> :: iterator it2 = sequence.begin();
+  
+  //vector<int> :: iterator it1 = array.begin(), it2 = sequence.begin();
+  auto it1 = array.begin(), it2 = sequence.begin();
 
   while ( it1 != array.end() &&  it2 != sequence.end() ){
-    if((*it1) == (*it2))
+    if( *it1 == *it2 )
       it2++;
-  
     it1++;
   }
-
-  if (it2 == sequence.end())
-    return true;
-  else
-    return false;
-
+  return (it2 == sequence.end());
 }
